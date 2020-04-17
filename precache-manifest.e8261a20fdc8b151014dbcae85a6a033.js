3fbf6d62cb2f34bafcd7dff5b3ea68655d460aef
SoyOscarRH <soyoscarrh@gmail.com>
2020-04-17 02:53:53 -0500

    Updates

---
+++ precache-manifest.e8261a20fdc8b151014dbcae85a6a033.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5d3de5cacd0d2c18eb",
+    "url": "/c93f0d15b9927a128650.module.wasm"
+  },
+  {
+    "revision": "0a43a144f6606dc778832ab4c8d1d1ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5d3de5cacd0d2c18eb",
+    "url": "/static/css/2.1c23bce0.chunk.css"
+  },
+  {
+    "revision": "dd580ae6fe6674aebdc6",
+    "url": "/static/css/main.da3023df.chunk.css"
+  },
+  {
+    "revision": "ca5d3de5cacd0d2c18eb",
+    "url": "/static/js/2.2261d8bf.chunk.js"
+  },
+  {
+    "revision": "95b1bca7217c431710cb",
+    "url": "/static/js/3.1af3b245.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/3.1af3b245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd580ae6fe6674aebdc6",
+    "url": "/static/js/main.35d7ad32.chunk.js"
+  },
+  {
+    "revision": "8615696ce7da7822534e",
+    "url": "/static/js/runtime-main.dd1e4587.js"
+  }
+]);
